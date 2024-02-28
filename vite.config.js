0f1c3b70c333d562ddacc800601129101f47168f
Kettei <97405768+KetteiPrOt@users.noreply.github.com>
2024-02-28 15:49:58 -0500

    Bug Fixed

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,11 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/css/components/*/*.css',
-                'resources/js/components/*/*.js',
+                'resources/css/components/InvoiceNumberInput/styles.css',
+                'resources/js/components/InvoiceNumberInput/InvoiceNumber.js',
+                'resources/js/components/InvoiceNumberInput/main.js',
+                'resources/css/components/permissions/input/styles.css',
+                'resources/js/components/permissions/input/script.js'
             ],
             refresh: true,
         }),
